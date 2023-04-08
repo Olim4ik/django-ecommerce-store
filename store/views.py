3e558ab3c696d3b5ffa7a87eb47a697b0c5818f4
Olim4ik <oakhmadjonov.uz@gmail.com>
2023-04-08 15:00:22 +0500

    store products detail page

--- store/views.py
+++ store/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from store.models import Category, Product
-
 
 
 def categories(request):
@@ -15,3 +14,10 @@ def all_products(request):
     
     context = {'products': products}
     return render(request, 'store/home.html', context)
+
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+
+    context = {'product': product}
+    return render(request, 'store/products/detail.html', context)
